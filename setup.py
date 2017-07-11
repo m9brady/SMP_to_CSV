3c5b4a6c7fe7699248e1f1024e25f5cd946f2662
m9brady <m9brady@gmail.com>
2017-07-11 16:26:25 -0400

    figuring out how to package this

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+cdir = path.abspath(path.dirname(__file__))
+
+setup(
+	name='SnowMicroToolset',
+	version='0.0.1',
+	description='Python tools for processing SMP observations',
+	long_description='',
+	url='https://github.com/m9brady/SMP_to_CSV',
+	author='Climate Processes Section',
+	author_email='',
+	license='Open Government Licence – Canada',
+	
+	classifiers=[
+		'Development Status :: 2 - Pre-Alpha',
+		'Intended Audience :: Science/Research',
+		'Topic :: Scientific/Engineering',
+		'License :: Other/Proprietary License',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+	],
+	
+	keywords='snow smp snowmicropentrometer',
+	
+	packages=find_packages(exclude=['contrib','docs','tests']),
+	
+	# run-time dependencies
+	install_requires=[
+		'numpy',
+		'scipy'
+		'pandas',
+		'matplotlib'
+	],
+	
+	python_requires='~=2.7',
+	
+	# additional dependencies
+	extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },	
+)
